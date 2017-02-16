3f1fb387847b687936058a38169e02e1579eabf9
Brian <brian.webb8@yahoo.com>
2017-02-16 11:25:00 -0600

    Spring boot initializer

---
+++ src/main/java/com/artiscene/ArtisceneApplication.java
@@ -0,0 +1,12 @@
+package com.artiscene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisceneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisceneApplication.class, args);
+	}
+}
