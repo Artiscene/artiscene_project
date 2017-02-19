966610c6cdc6a2dbe062b1b2010386fb0c575baf
haliekoehler <koehlerdoom@gmail.com>
2017-02-19 15:17:18 -0600

    Register & Logout Page created authentication controller added register.html w/ form added login.html

---
+++ src/main/java/com/artiscene/controllers/FormsController.java
@@ -0,0 +1,18 @@
+package com.artiscene.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by HKoehler on 2/17/17.
+ */
+@Controller
+public class FormsController {
+
+    @GetMapping("/forms/register")
+    public String registerPage(Model model){
+        return "forms/register";
+    }
+
+}
