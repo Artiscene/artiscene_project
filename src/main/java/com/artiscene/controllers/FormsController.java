884c929797712b259aa70a41ea6a527fd4839975
haliekoehler <koehlerdoom@gmail.com>
2017-02-22 12:02:30 -0600

    Upload Form Styled added to FormsController Styled Forms / made classes Added For Sale -> Price input toggle feature Added "Add Artwork" button to append another upload div (Has bugs)

--- src/main/java/com/artiscene/controllers/FormsController.java
+++ src/main/java/com/artiscene/controllers/FormsController.java
@@ -11,29 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FormsController {
 
 
-    @GetMapping("/forms/register")
-    public String registerPage(Model model){
-        return "forms/register";
-    }
-
-    @GetMapping("/forms/login")
-    public String loginPage(Model model){
-
-    @GetMapping("/register")
+    @GetMapping("forms/register")
     public String registerPage(){
         return "forms/register";
     }
 
-    @GetMapping("/login")
+    @GetMapping("forms/login")
     public String loginPage(){
 
         return "forms/login";
     }
 
-    @GetMapping("/forms/upload")
-    public String uploadPage(Model model) { return "forms/upload"; }
+    @GetMapping("forms/upload")
+    public String uploadPage() { return "forms/upload"; }
 
-    @GetMapping("/forms/purchase")
+    @GetMapping("forms/purchase")
     public String purchasePage(Model model) { return "forms/purchase"; }
 
 }
