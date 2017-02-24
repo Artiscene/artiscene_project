b5ddbf5dfeeb101692ac2f6652995132ddcd1765
haliekoehler <koehlerdoom@gmail.com>
2017-02-24 15:02:11 -0600

    Brought back ForumController Brought back forum.html Forum post Modal Fixed nav bar - take out Home, Link Forum Portfolio shows up if logged in Upload Modal working, shows preview but does not upload yet

--- src/main/java/com/artiscene/controllers/PortfolioController.java
+++ src/main/java/com/artiscene/controllers/PortfolioController.java
@@ -1,7 +1,10 @@
 package com.artiscene.controllers;
 
+import com.artiscene.models.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
  * Created by HKoehler on 2/17/17.
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PortfolioController {
 
+
     @GetMapping("/portfolio")
-    public String portfolioPage(){
+    public String uploadModal(@ModelAttribute Project project, Model model) {
+        model.addAttribute("project", project);
         return "portfolio";
     }
+
+
 }
