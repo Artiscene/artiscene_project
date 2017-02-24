9413ec456792a13b1fe6edd2b9ceec84ca647116
Brian <brian.webb8@yahoo.com>
2017-02-24 15:05:03 -0600

    gallery page produces artwork, make changes to portfolio stuff

--- src/main/java/com/artiscene/controllers/PortfolioController.java
+++ src/main/java/com/artiscene/controllers/PortfolioController.java
@@ -1,7 +1,15 @@
 package com.artiscene.controllers;
 
+import com.artiscene.models.User;
+import com.artiscene.repositories.ProjectRepository;
+import com.artiscene.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by HKoehler on 2/17/17.
@@ -10,8 +18,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PortfolioController {
 
+    @Autowired
+    public PortfolioController(ProjectRepository repository) {
+        this.repository = repository;
+    }
+
+    private ProjectRepository repository;
+
     @GetMapping("/portfolio")
-    public String portfolioPage(){
+    public String portfolioPage(Model model){
+        User user =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("projects", repository.findByUser(user));
+        return "portfolio";
+    }
+
+    @GetMapping("/portolio/{id}")
+    public String userPortfolioPage(Model model, @PathVariable Long id){
+        repository.findByUserId(id);
+        model.addAttribute("projects", repository.findByUserId(id));
         return "portfolio";
     }
 }
