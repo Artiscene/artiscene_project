23445f34d0e6379dc50d050fa1aab22dc24c2fe8
Brian <brian.webb8@yahoo.com>
2017-03-07 13:57:47 -0600

    changes to get ready for deployment

--- src/main/java/com/artiscene/ArtisceneApplication.java
+++ src/main/java/com/artiscene/ArtisceneApplication.java
@@ -2,11 +2,19 @@ package com.artiscene;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ArtisceneApplication {
+public class ArtisceneApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtisceneApplication.class, args);
 	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ArtisceneApplication.class);
+    }
 }
+
+
