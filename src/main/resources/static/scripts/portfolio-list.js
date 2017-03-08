02b10cbd898a0245b9cc72b81ee1aa1b964b75d9
haliekoehler <koehlerdoom@gmail.com>
2017-03-08 09:06:10 -0600

    Working on gallery search bar responsiveness Styling profile info-div

--- src/main/resources/static/scripts/portfolio-list.js
+++ src/main/resources/static/scripts/portfolio-list.js
@@ -2,13 +2,16 @@
  * Created by Brian on 2/27/17.
  */
 (function () {
+
+    console.log("JS portfolio is working");
+
     var user_id=$("#user_id").val();
     var request = $.ajax({
 
         url: '/getportfolio.json',
         data:{id:user_id}
     });
-    console.log("JS portfolio is working");
+
     request.done(function (projects) {  // the HTTP response -> an array of JSON objects -> ads
         var i, html = '';
 
@@ -20,4 +23,11 @@
 
         $('#load-portfolio').html(html);
     });
+
+    $(window).resize(function(){
+        If($(window).width()<500){
+            $('.fade').removeClass('fade');
+        }
+    });
+
 })();
